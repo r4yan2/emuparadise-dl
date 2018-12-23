5ad23be5f36efd36461234bb69e13a907d7977f8
Andrea Grazioso <grazioandre@gmail.com>
2018-12-24 00:32:21 +0100

    Added setup.py for PyPI build

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+from setuptools import setup, find_packages
+setup(
+    name="emuparadise-dl",
+    version="0.1",
+    packages=['emuparadise_dl'],
+    scripts=['bin/emuparadise-dl'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=[
+        'requests',
+        'bs4',
+        'termcolor',
+        ],
+
+    # metadata to display on PyPI
+    author="Andrea Grazioso (ray)",
+    author_email="grazioandre@gmail.com",
+    description="Search and Download roms from hosting website",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="rom emulator retrogaming",
+    url="https://github.com/r4yan2/emuparadise-dl",   # project home page, if any
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Licence :: OSI Approved :: MIT License",
+        "Operating System :: OS Indipendent"
+        ]
+)
